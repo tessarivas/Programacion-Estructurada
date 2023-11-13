66133d321f1ba663e1a0b7ae13fd1f88af00ef96
Teresa Rivas <141757999+tessarivas@users.noreply.github.com>
2023-11-12 18:48:41 -0800

    act 12

---
+++ ACTIVIDAD 12/caramelo.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX 50
+
+int main(int args, char *arg[]) 
+{
+    FILE *archivo;
+    int posicion = 0;
+    char nombre_archivo[MAX];
+    int numero;
+    int matricula;
+    char nombre[20], apellido1[20], apellido2[20];
+    int edad;
+    char genero[10];
+
+    strcpy(nombre_archivo, arg[1]);
+    strcat(nombre_archivo, ".txt");
+
+    archivo = fopen(nombre_archivo, "r");
+
+    if (archivo) 
+    {
+        char linea[1000];
+
+        while (fgets(linea, sizeof(linea), archivo) != NULL) 
+        {
+            if (sscanf(linea, "%d.- %d %s %s %s %d %s", &numero, &matricula, nombre, apellido1, apellido2, &edad, genero) == 7) 
+            {
+                posicion++;
+            }
+        }
+
+        fclose(archivo);
+        return posicion;
+    } 
+    else 
+    {
+        printf("Error: No se pudo abrir el archivo %s\n", nombre_archivo);
+        return -1;
+    }
+
+    return 0;
+}
